3d6a0a0ef5c3d5f57070179608891b6e73fb00d7
Brad Urani <bradurani@gmail.com>
2016-08-11 01:47:25 -0700

    Add yml parser

--- gitfile/gitfile.go
+++ gitfile/gitfile.go
@@ -2,32 +2,62 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
+
+type repo struct {
+	Url    string
+	Path   string
+	Branch string
+	Tag    string
+}
 
 func main() {
 	argsWithProg := os.Args
-	absPath := AbsPath(argsWithProg)
+	absPath := absPath(argsWithProg)
 	fmt.Println(absPath)
+	contents := readFile(absPath)
+	repos := parseFile(contents)
+	fmt.Println(repos)
 }
 
-func AbsPath(argsWithProg []string) string {
+func readFile(absPath string) string {
+	dat, err := ioutil.ReadFile(absPath)
+	check(err)
+	return string(dat)
+}
+
+func parseFile(contents string) []repo {
+	r := []repo{}
+	err := yaml.Unmarshal([]byte(contents), &r)
+	check(err)
+	fmt.Printf("--- t:\n%v\n\n", r)
+	return r
+}
+
+func absPath(argsWithProg []string) string {
 	dirArg := "."
 	if len(argsWithProg) > 1 {
 		dirArg = argsWithProg[1]
 	}
-	currentDir := CurrentDir()
+	currentDir := currentDir()
 	absPath := path.Join(currentDir, dirArg, "Gitfile")
 	return absPath
 }
 
-func CurrentDir() string {
+func currentDir() string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	return dir
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
