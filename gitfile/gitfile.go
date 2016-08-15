d45698e854b80c2c343e4c2794f87d8cb036bb1e
Brad Urani <bradurani@gmail.com>
2016-08-14 17:09:04 -0700

    Pull repo and repo defaults

--- gitfile/gitfile.go
+++ gitfile/gitfile.go
@@ -18,16 +18,32 @@ type repo struct {
 	Path   string
 	Branch string
 	Tag    string
+	Commit string
 }
 
 func main() {
 	argsWithProg := os.Args
-	absPath := absPath(argsWithProg)
-	fmt.Println(absPath)
-	contents := readFile(absPath)
+	gitfilePath := gitfilePath(argsWithProg)
+	fmt.Println(gitfilePath)
+	contents := readFile(gitfilePath)
 	repos := parseFile(contents)
+	addRepoDefaults(repos)
 	fmt.Println(repos)
 	updateRepos(repos)
+}
+
+func addRepoDefaults(repos []repo) {
+	for i := range repos {
+		if repos[i].Url == "" {
+			panic("repos must have url field")
+		}
+		if repos[i].Path == "" {
+			repos[i].Path = "."
+		}
+		if repos[i].Branch == "" && repos[i].Tag == "" && repos[i].Commit == "" {
+			repos[i].Branch = "master"
+		}
+	}
 }
 
 func updateRepos(repos []repo) {
@@ -46,25 +62,46 @@ func updateRepo(repo repo) {
 	repoExists := repoExists(path, gitDir)
 	fmt.Println("exists: ", repoExists)
 	if repoExists {
-		pullRepo(repo, path)
+		pullRepo(repo, gitDir)
 	} else {
 		cloneRepo(repo, path)
 	}
 }
 
-func pullRepo(repo repo, path string) {
+func pullRepo(repo repo, gitDir string) {
+	pwd := getPwd()
+	fmt.Println("pwd: ", pwd)
+	changeDir(gitDir)
+	args := []string{"pull", "--ff-only"}
+	runCmd("/usr/local/bin/git", args)
+	changeDir(pwd)
+}
 
+func changeDir(p string) {
+	err := os.Chdir(p)
+	check(err)
+}
+
+func getPwd() string {
+	pwd, err := os.Getwd()
+	check(err)
+	return pwd
 }
 
 func cloneRepo(repo repo, path string) {
-	args := []string{"clone", repo.Url, repo.Path}
+	args := []string{"clone", repo.Url}
+	fmt.Println("path:", repo.Path)
+	if strings.TrimSpace(repo.Path) != "" {
+		args = append(args, repo.Path)
+	}
 	runCmd("/usr/local/bin/git", args)
 }
 
 func runCmd(cmd string, args []string) {
+	fmt.Println("cmd: ", strings.Join(append([]string{cmd}, args...), " "))
 	out, err := exec.Command(cmd, args...).Output()
 	check(err)
-	fmt.Printf("%s", out)
+	fmt.Println(out)
 }
 
 func parseGitDir(repoUrl string) (gitDir string) {
@@ -75,13 +112,15 @@ func parseGitDir(repoUrl string) (gitDir string) {
 	if len(segments) != 2 {
 		panic("urls must have 2 path segments")
 	}
-	return segments[1]
+	lastSegment := strings.TrimRight(segments[1], ".git")
+	return lastSegment
 }
 
 func repoExists(repoPath string, gitDir string) (exists bool) {
 	err := os.MkdirAll(repoPath, 0777)
 	check(err)
 	gitPath := filepath.Join(repoPath, gitDir, ".git")
+	fmt.Println("path exists? ", gitPath)
 	pathExists, err := pathExists(gitPath)
 	check(err)
 	return pathExists
@@ -111,7 +150,7 @@ func parseFile(contents string) []repo {
 	return r
 }
 
-func absPath(argsWithProg []string) string {
+func gitfilePath(argsWithProg []string) string {
 	dirArg := "."
 	if len(argsWithProg) > 1 {
 		dirArg = argsWithProg[1]
@@ -129,6 +168,7 @@ func currentDir() string {
 
 func check(e error) {
 	if e != nil {
+		fmt.Println(e)
 		panic(e)
 	}
 }
